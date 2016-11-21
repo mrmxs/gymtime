3085ad8be513380e9bf4d53e3a9c4456af4b0c38
mrmxs <mrmxs@users.noreply.github.com>
2016-11-21 03:14:37 +0300

    add creating 'users' table

--- create_gymtime_db.sql
+++ create_gymtime_db.sql
@@ -19,3 +19,37 @@ INSERT INTO gyms (name, about) VALUES
   ('Фитнес клуб "Олимпик Холл"', 'https://vk.com/kostromaolympichall'),
   ('Аура_Фитнес-клуб_Кострома', 'https://vk.com/aurakostroma'),
   ('Фитнес клуб "АПЕЛЬСИН" Кострома', 'https://vk.com/fc.apelsin');
+  
+  
+/**
+ * Таблица 'users'
+ * пользователи
+ */
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,            -- идентификатор пользователя
+  name varchar(100) NOT NULL,       -- ФИ пользователя
+  -- first_name varchar(50) NOT NULL,  -- имя пользователя         
+  -- last_name varchar(50) NOT NULL,   -- фамилия пользователя
+  sex char(1),                      -- пол: 'F' for female, 'M' for male
+  birthday date,                    -- дата рождения
+  e_mail varchar(50) NOT NULL,      -- адрес электронной почты
+  phone varchar(10),                -- телефон
+  avatar varchar(100)               -- название файла аватара
+);
+
+-- COPY users FROM 'users.txt';
+INSERT INTO users (birthday, e_mail, name) VALUES 
+  ('1982-02-03', 'jjivjuthyid@xaker.ru', 'Шалимов Игорь'),
+  ('1993-04-23', 'ruchiashyich@usa.net', 'Купревича Ольга'),
+  ('1990-01-16', 'chehy@mail.ru', 'Касьяненко Владимир'),
+  ('1982-11-05', 'routhe@softhome.com', 'Белова Юлия'),
+  ('1993-04-17', 'pyasy@umr.ru', 'Пронин Евгений');
+INSERT INTO users (birthday, e_mail, name, sex) VALUES 
+  ('1982-09-01', 'nixu@email.ru', 'Федулова Оксана', 'F'),
+  ('1992-11-05', 'shjivya@rol.ru', 'Янчуров Дмитрий', 'M'),
+  ('1981-10-19', 'xjuvyeziex@rin.ru', 'Полунина Лидия', 'F'),
+  ('1978-03-19', 'fjihyexio@email.ru', 'Контеенко Василий', 'M'),
+  ('1984-10-15', 'shjiqu@land.ru', 'Наумова Кристина', 'F');
